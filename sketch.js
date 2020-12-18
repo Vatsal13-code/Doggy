71d8cdc4497e998c60311832d3cd4bfbe2d09e47
Vatsal13-code <69814730+Vatsal13-code@users.noreply.github.com>
2020-12-17 20:06:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+//Create variables here
+var dog, dogImg, happyDog, database, foodS, foodStock
+
+function preload()
+{
+
+  //load images here
+  happyDog = loadImage("dogImg1.png")
+  dogImg = loadImage("dogImg.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog = createSprite(250,250,10,10)
+  dog.addImage(dogImg);
+  database = firebase.database();
+  foodStock=database.ref('Food')
+  foodStock.on("value",readStock)
+  dog.scale = 0.5
+}
+
+
+function draw() {  
+background(46,139,87)
+  
+if(keyWentDown(UP_ARROW)) {
+  writeStock(foodS)
+  dog.addImage(happyDog)
+}
+
+text("Food remaining : "+foodS,170,200);  
+text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+textSize(13);
+
+  drawSprites();
+  dog.display();
+}
+
+function writeStock(x) {
+
+  if( x<=0 ) {
+    x=0
+  } else {
+    x=x-1
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+function readStock(data) {
+foodS=data.val();
+}
